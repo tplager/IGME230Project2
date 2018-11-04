9fc258f8c20a5e188c7bc713ac3cc10c1512771d
Trenton <tplager1@gmail.com>
2018-11-04 13:30:18 -0500

    Added script import statements

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = (e) => {document.querySelector("#search").onclick = getData};
+	
+// 2
+let displayTerm = "";
+
+// 3
+function getData(){
+    const OMDB_URL = "http://www.omdbapi.com/?apikey=[fbf8855f]&";
+
+    
+}
